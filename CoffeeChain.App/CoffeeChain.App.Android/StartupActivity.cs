fa2f3a9ad9379d3510405c769b4eb200e6d504b9
Namoshek <marvin-mall@msn.com>
2018-05-31 21:57:55 +0200

    remove delay in startup activity for android

--- CoffeeChain.App/CoffeeChain.App.Android/StartupActivity.cs
+++ CoffeeChain.App/CoffeeChain.App.Android/StartupActivity.cs
@@ -30,7 +30,7 @@ namespace CoffeeChain.App.Droid
         async void SimulateStartup()
         {
             Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
-            await Task.Delay(1000); // Simulate a bit of startup work.
+            //await Task.Delay(1000); // Simulate a bit of startup work.
             Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
