52fbc9df7fc0999729923bd093b3c1df5a7c6e78
Namoshek <marvin-mall@msn.com>
2018-05-31 21:57:46 +0200

    implements a table style transaction page for coffee buyings

---
+++ CoffeeChain.App/CoffeeChain.App/ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoffeeChain.Connector;
+using CoffeeChain.Connector.FunctionOutputs;
+using Nethereum.Contracts;
+using Nethereum.Web3;
+
+namespace CoffeeChain.App.ViewModels
+{
+    public class TransactionsViewModel : BaseViewModel
+    {
+        private readonly Web3 _web3;
+        private readonly ICoffeeEconomyService _coffeeEconomyService;
+
+        private IList<EventLog<CoffeeBoughtEvent>> _transactions = new List<EventLog<CoffeeBoughtEvent>>();
+        public IList<EventLog<CoffeeBoughtEvent>> Transactions
+        {
+            get { return _transactions; }
+            set { SetProperty(ref _transactions, value); }
+        }
+
+        public TransactionsViewModel(Web3 web3, ICoffeeEconomyService coffeeEconomyService)
+        {
+            _web3 = web3;
+            _coffeeEconomyService = coffeeEconomyService;
+
+            Title = "Transaktionen";
+            IsBusy = true;
+        }
+
+        public async Task OnAppearing()
+        {
+            Transactions = await _coffeeEconomyService.GetCoffeeBoughtEventsForWallet(_web3.TransactionManager.Account.Address);
+
+            IsBusy = false;
+        }
+    }
+}
--- CoffeeChain.App/CoffeeChain.App/Views/TransactionsPage.xaml.cs
+++ CoffeeChain.App/CoffeeChain.App/Views/TransactionsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using CoffeeChain.App.ViewModels;
+using CommonServiceLocator;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CoffeeChain.App.Views
@@ -6,9 +8,20 @@ namespace CoffeeChain.App.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TransactionsPage : ContentPage
     {
+        TransactionsViewModel _viewModel;
+
         public TransactionsPage()
         {
             InitializeComponent();
+
+            _viewModel = ServiceLocator.Current.GetInstance<TransactionsViewModel>();
+
+            BindingContext = _viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            await _viewModel.OnAppearing();
         }
     }
 }
