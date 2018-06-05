190aee5e03cec119f5fe78378b76392b193975b7
Marvin Mall <marvin.mall@energie4all.at>
2018-06-05 16:22:05 +0200

    add string helper to check if string is valid wallet

--- CoffeeChain.App/CoffeeChain.App/StringExtensions.cs
+++ CoffeeChain.App/CoffeeChain.App/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace CoffeeChain.App
+﻿using System.Text.RegularExpressions;
+using Nethereum.Util;
+
+namespace CoffeeChain.App
 {
     public static class StringExtensions
     {
@@ -10,6 +13,26 @@
             }
 
             return false;
+        }
+
+        public static bool IsValidEthereumAddress(this string address)
+        {
+            Regex r = new Regex("^(0x){1}[0-9a-fA-F]{40}$");
+            // Does not match length, prefix or hex
+            if (!r.IsMatch(address))
+            {
+                return false;
+            }
+            // It is all lowercase, so no checksum needed
+            else if (address == address.ToLower())
+            {
+                return true;
+            }
+            // Do checksum
+            else
+            {
+                return new AddressUtil().IsChecksumAddress(address);
+            }
         }
     }
 }
